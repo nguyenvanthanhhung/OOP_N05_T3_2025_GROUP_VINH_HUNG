9ced052fdf7e2175ee0ea549716612825d4968b9
nguyenvanthanhhung <24100101@st.phenikaa-uni.edu.vn>
2025-07-15 17:58:23 +0700

    Update NhanVien.java

--- ứng dụng quản lý quán coffee/src/NhanVien.java
+++ ứng dụng quản lý quán coffee/src/NhanVien.java
@@ -1,44 +1,14 @@
 public class NhanVien {
-    private String id;
-    private String ten;
-    private String vaiTro;
-    private String caLam;
+    String id, ten, vaiTro, ca;
 
-    public NhanVien(String id, String ten, String vaiTro, String caLam) {
+    public NhanVien(String id, String ten, String vaiTro, String ca) {
         this.id = id;
         this.ten = ten;
         this.vaiTro = vaiTro;
-        this.caLam = caLam;
+        this.ca = ca;
     }
 
-    public String getId() {
-        return id;
-    }
-    public String getTen() {
-        return ten;
-    }
-    public String getVaiTro() {
-        return vaiTro;
-    }
-    public String getCaLam() {
-        return caLam;
-    }
-
-    public void setTen(String ten) {
-        this.ten = ten;
-    }
-    public void setVaiTro(String vaiTro) {
-        this.vaiTro = vaiTro;
-    }
-    public void setCaLam(String caLam) {
-        this.caLam = caLam;
-    }
-
-    public void hienThi() {
-        System.out.println("=== THONG TIN NHAN VIEN ===");
-        System.out.println("ID      : " + id);
-        System.out.println("Ten     : " + ten);
-        System.out.println("Vai tro : " + vaiTro);
-        System.out.println("Ca lam  : " + caLam);
+    void hienThi() {
+        System.out.println(id + " | " + ten + " | " + vaiTro + " | " + ca);
     }
 }
