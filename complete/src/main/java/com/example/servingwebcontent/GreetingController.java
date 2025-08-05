2bfb72bf0cb3798b8f89a4db4ca540cca133f6eb
nguyenvanthanhhung <24100101@st.phenikaa-uni.edu.vn>
2025-08-05 11:22:34 +0000

    update

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,9 +1,13 @@
 package com.example.servingwebcontent;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.servingwebcontent.Model.KhachHang;
 
 @Controller
 public class GreetingController {
@@ -12,6 +16,21 @@ public class GreetingController {
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
+	}
+
+	@GetMapping("/khachang")
+	public String myKhachhang(Model model) {
+
+		KhachHang kh = new KhachHang(01, "Nguyen Thi Van", "021098883848");
+         KhachHang kh2 = new KhachHang(02, "Nguyen Tran Dung", "021098883848");
+		 ArrayList<KhachHang> list = new ArrayList<KhachHang>();
+		 list.add(kh);
+		 list.add(kh2);
+		 
+
+		model.addAttribute("myKH", kh);
+		model.addAttribute("mylist", list);
+		return "myKhachHang";
 	}
 
 }
---
+++ complete/src/main/java/com/example/servingwebcontent/Model/KhachHang.java
@@ -0,0 +1,22 @@
+package com.example.servingwebcontent.Model;
+
+public class KhachHang {
+
+    public int id;
+    public String ten;
+    public String soDienThoai;
+
+    public KhachHang(int id, String ten, String soDienThoai) {
+        this.id = id;
+        this.ten = ten;
+        this.soDienThoai = soDienThoai;
+    }
+
+    public void hienThi() {
+        System.out.println("ID: " + id + ", Ten: " + ten + ", SDT: " + soDienThoai);
+    }
+    public String getTenKh(){
+        return ten;
+    }
+}
+
